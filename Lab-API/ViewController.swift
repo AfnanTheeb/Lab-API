36f9775d41c25b10a1b5d1fd24a836d448acd6ec
AfnanTheb <Afnan.Alotaibi.med.s@tuwaiq.edu.sa>
2021-11-14 23:52:22 +0300

    end

--- Lab-API/ViewController.swift
+++ Lab-API/ViewController.swift
@@ -7,13 +7,62 @@
 
 import UIKit
 
+class Characters : Codable {
+    var characters : [Character]
+}
+struct Character : Codable {
+    var name : String
+    var role : String
+   
+}
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        getCharacters()
     }
+    func getCharacters(){
+        // https://api.jikan.moe/v3/anime/1/characters_staff
+        
+        var urlAnimeCharacters = URLComponents()
+        urlAnimeCharacters.scheme = "https"
+        urlAnimeCharacters.host = "api.jikan.moe"
+        urlAnimeCharacters.path = "/v3/anime/1/characters_staff"
+        
+        
+        let animeCharactersQuery = URLQueryItem(name: "q", value: "")
+        let animeCharactersQuery2 = URLQueryItem(name: "order_by", value: "members")
+        let animeCharactersQuery3 = URLQueryItem(name: "sort", value: "desc")
+        let animeCharactersQuery4 = URLQueryItem(name: "page", value: "1")
 
+        urlAnimeCharacters.queryItems = [animeCharactersQuery, animeCharactersQuery2, animeCharactersQuery3, animeCharactersQuery4]
+        
+        
+      
+        let urlSession = URLSession.shared
+        
+        
+        let animeCharactersRequest = URLRequest(url: urlAnimeCharacters.url! )
+
+        
+        let fetchDataTask = urlSession.dataTask(with: animeCharactersRequest) {
+            (data: Data?, response: URLResponse?, err: Error?) in
+            if (err == nil ){
+                do {
+                    let jsonDecoder = JSONDecoder()
+                    let charactersResponse = try jsonDecoder.decode(Characters.self, from: data!)
+                    print(charactersResponse.characters)
+                } catch {
+                    print("err: \(err)")
+                }
+                // Print data in string format
+//                print (String(data: data!, encoding: .utf8))
+            }
+        }
+        
+        fetchDataTask.resume()
+    }
 
 }
 
